eec288b0e57cf28deae0ab046a4d151673e9f273
Jason Vann <vannjt1@icloud.com>
2021-10-05 18:52:18 -0400

    Setup Redux

--- src/index.js
+++ src/index.js
@@ -9,9 +9,14 @@ import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+
+const store = createStore(reducer);
+
 ReactDOM.render(
   <Router>
+    <Provider>
       <App />
+    </Provider>
   </Router>,
   document.getElementById('root')
 );
